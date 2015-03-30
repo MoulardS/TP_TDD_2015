abafc0ce7ee0482656189507b471d8403cf43024
MoulardS <fappingmaterial@hotmail.fr>
2015-03-30 14:49:36 +0200

    Juste return zéro

---
+++ src/main/java/iut/tdd/Convert.java
@@ -0,0 +1,15 @@
+package iut.tdd;
+
+import java.util.HashMap;
+
+public class Convert {
+	
+	public static HashMap<String,String> liste = new HashMap<String,String>();	
+	
+	public static String num2text(String input) {	
+		return "zéro";
+	}
+	public static String text2num(String input) {
+		return null;
+	}
+}
